7ec4535f805c5d4441d9ab403d2faa752a721cfb
Johan Kjellström <48734017+nahrr@users.noreply.github.com>
2022-06-16 00:48:42 +0200

    refactor css & timer

--- src/timers/ZulAman.tsx
+++ src/timers/ZulAman.tsx
@@ -1,27 +1,42 @@
 import { useEffect, useState } from "react";
 import { useTimer } from "../hooks/timer";
 import style from "./Timers.module.css";
-
 const ZulAman = () => {
-  const [date, setDate] = useState<Date>(new Date("2022-06-14T08:00:00"));
-  const { days, hours, minutes, seconds, isTimerFinished } = useTimer(date);
+  const [dateTime, setDateTime] = useState<Date>(
+    new Date("2022-06-17T08:00:00")
+  );
+  const { days, hours, minutes, seconds, isTimerFinished } = useTimer(dateTime);
 
   useEffect(() => {
     if (isTimerFinished) {
-      const newDate = date?.setDate(date.getDate() + 3);
-      setDate(new Date(newDate));
+      setDateTime((d) => new Date(d.setDate(d.getDate() + 3)));
     }
-  }, [, isTimerFinished]);
+  }, [isTimerFinished]);
 
   const zeroPad = (num: number) => String(num).padStart(2, "0");
 
   return (
     <div className={style.za}>
       <div className={style.timers}>
-        <h1 className={style.heading}>
-          {days === 0 ? "" : days + " :"} {hours} : {minutes} :{" "}
-          {zeroPad(seconds)}
-        </h1>
+        <div className={style.firstRow}>
+          <span className={style.header}>Days</span>
+          <span></span>
+          <span className={style.header}>Hours</span>
+          <span></span>
+          <span className={style.header}>Minutes</span>
+          <span></span>
+          <span className={style.header}>Seconds</span>
+        </div>
+
+        <div className={style.secondRow}>
+          <span>{days}</span>
+          <span className={style.divider}>:</span>
+          <span>{hours}</span>
+          <span className={style.divider}>:</span>
+          <span>{minutes}</span>
+          <span className={style.divider}>:</span>
+          <span>{zeroPad(seconds)}</span>
+        </div>
       </div>
     </div>
   );
